58ab55a5da60a5e937d0a5e0a2f6b6504b3a8248
Zmulvihill <zmulvihi@yahoo.com>
2023-06-29 11:20:18 -0700

    added sound clip to audio class

--- src/App.js
+++ src/App.js
@@ -8,39 +8,39 @@ function App() {
         <div id="display">
         <div class="drum-pad" id="drum-pad1">
       <button class="pad" id="pad1">Q</button>
-      <audio class="sound" src="sound1.mp3"></audio>
+      <audio class="sound clip" id="Q" src="sound1.mp3"></audio>
     </div>
     <div class="drum-pad" id="drum-pad2">
       <button class="pad" id="pad2">W</button>
-      <audio class="sound" src="sound2.mp3"></audio>
+      <audio class="sound clip" id="W" src="sound2.mp3"></audio>
     </div>
     <div class="drum-pad" id="drum-pad3">
       <button class="pad" id="pad3">E</button>
-      <audio class="sound" src="sound3.mp3"></audio>
+      <audio class="sound clip" id="E" src="sound3.mp3"></audio>
     </div>
     <div class="drum-pad" id="drum-pad4">
       <button class="pad" id="pad4">A</button>
-      <audio class="sound" src="sound4.mp3"></audio>
+      <audio class="sound clip" id="A" src="sound4.mp3"></audio>
     </div>
     <div class="drum-pad" id="drum-pad5">
       <button class="pad" id="pad5">S</button>
-      <audio class="sound" src="sound5.mp3"></audio>
+      <audio class="sound clip" id="S" src="sound5.mp3"></audio>
     </div>
     <div class="drum-pad" id="drum-pad6">
       <button class="pad" id="pad6">D</button>
-      <audio class="sound" src="sound6.mp3"></audio>
+      <audio class="sound clip" id="D" src="sound6.mp3"></audio>
     </div>
     <div class="drum-pad" id="drum-pad7">
       <button class="pad" id="pad7">Z</button>
-      <audio class="sound" src="sound7.mp3"></audio>
+      <audio class="sound clip" id="Z" src="sound7.mp3"></audio>
     </div>
     <div class="drum-pad" id="drum-pad8">
       <button class="pad" id="pad8">X</button>
-      <audio class="sound" src="sound8.mp3"></audio>
+      <audio class="sound clip" id="X" src="sound8.mp3"></audio>
     </div>
     <div class="drum-pad" id="drum-pad9">
       <button class="pad" id="pad9">C</button>
-      <audio class="sound" src="sound9.mp3"></audio>
+      <audio class="sound clip" id="C" src="sound9.mp3"></audio>
     </div>
         </div>
       </div>
