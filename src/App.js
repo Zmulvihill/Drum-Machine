50223d8a8b64777be09a9ac233ef17b1e17072f2
Zmulvihill <zmulvihi@yahoo.com>
2023-07-01 10:57:59 -0700

    edited App and saved

--- src/App.js
+++ src/App.js
@@ -1,70 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <DrumMachine />
+      <div id="drum-machine">
+        <div id="display">
+        <div class="drum-pad" id="drum-pad1">
+      <button class="pad" id="pad1">Q</button>
+      <audio class="sound clip" id="Q" src="sound1.mp3"></audio>
     </div>
-  );
-};
-
-const DrumMachine = () => {
-  const [displayText, setDisplayText] = useState('');
-
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      const key = event.key.toUpperCase();
-      const drumPad = document.getElementById(`drum-pad${key}`);
-      if (drumPad) {
-        const audio = drumPad.querySelector('audio');
-        audio.currentTime = 0;
-        audio.play();
-        setDisplayText(`Triggered: ${key}`);
-      }
-    };
-
-    document.addEventListener('keydown', handleKeyPress);
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyPress);
-    };
-  }, []);
-
-  return (
-    <div id="drum-machine">
-      <h1>Welcome to the Drum Machine!</h1>
-      <div id="display">{displayText}</div>
-      <DrumPad triggerKey="Q" soundSrc="sound1.mp3" />
-      <DrumPad triggerKey="W" soundSrc="sound2.mp3" />
-      <DrumPad triggerKey="E" soundSrc="sound3.mp3" />
-      <DrumPad triggerKey="A" soundSrc="sound4.mp3" />
-      <DrumPad triggerKey="S" soundSrc="sound5.mp3" />
-      <DrumPad triggerKey="D" soundSrc="sound6.mp3" />
-      <DrumPad triggerKey="Z" soundSrc="sound7.mp3" />
-      <DrumPad triggerKey="X" soundSrc="sound8.mp3" />
-      <DrumPad triggerKey="C" soundSrc="sound9.mp3" />
+    <div class="drum-pad" id="drum-pad2">
+      <button class="pad" id="pad2">W</button>
+      <audio class="sound clip" id="W" src="sound2.mp3"></audio>
     </div>
-  );
-};
-
-const DrumPad = ({ triggerKey, soundSrc }) => {
-  const handlePadClick = () => {
-    const audio = document.getElementById(triggerKey);
-    if (audio) {
-      audio.currentTime = 0;
-      audio.play();
-    }
-  };
-
-  return (
-    <div className="drum-pad" id={`drum-pad${triggerKey}`}>
-      <button className="pad" onClick={handlePadClick}>
-        {triggerKey}
-      </button>
-      <audio className="sound clip" id={triggerKey} src={soundSrc} />
+    <div class="drum-pad" id="drum-pad3">
+      <button class="pad" id="pad3">E</button>
+      <audio class="sound clip" id="E" src="sound3.mp3"></audio>
     </div>
+    <div class="drum-pad" id="drum-pad4">
+      <button class="pad" id="pad4">A</button>
+      <audio class="sound clip" id="A" src="sound4.mp3"></audio>
+    </div>
+    <div class="drum-pad" id="drum-pad5">
+      <button class="pad" id="pad5">S</button>
+      <audio class="sound clip" id="S" src="sound5.mp3"></audio>
+    </div>
+    <div class="drum-pad" id="drum-pad6">
+      <button class="pad" id="pad6">D</button>
+      <audio class="sound clip" id="D" src="sound6.mp3"></audio>
+    </div>
+    <div class="drum-pad" id="drum-pad7">
+      <button class="pad" id="pad7">Z</button>
+      <audio class="sound clip" id="Z" src="sound7.mp3"></audio>
+    </div>
+    <div class="drum-pad" id="drum-pad8">
+      <button class="pad" id="pad8">X</button>
+      <audio class="sound clip" id="X" src="sound8.mp3"></audio>
+    </div>
+    <div class="drum-pad" id="drum-pad9">
+      <button class="pad" id="pad9">C</button>
+      <audio class="sound clip" id="C" src="sound9.mp3"></audio>
+    </div>
+        </div>
+      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Drum Machine
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+
   );
-};
+}
 
 export default App;
