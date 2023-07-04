d8d3580dd775490db64cfe2b5bec472074a2e327
Zmulvihill <zmulvihi@yahoo.com>
2023-07-04 08:44:05 -0700

    deleted extra audioclips and saved

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from './logo.svg';
 import './App.css';
 
 const audioClips=[{
@@ -46,61 +47,13 @@ const audioClips=[{
     id: "Closed-HH",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
 }]
-  , d = [{
-    keyCode: 81,
-    keyTrigger: "Q",
-    id: "Chord-1",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
-}, {
-    keyCode: 87,
-    keyTrigger: "W",
-    id: "Chord-2",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
-}, {
-    keyCode: 69,
-    keyTrigger: "E",
-    id: "Chord-3",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
-}, {
-    keyCode: 65,
-    keyTrigger: "A",
-    id: "Shaker",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
-}, {
-    keyCode: 83,
-    keyTrigger: "S",
-    id: "Open-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
-}, {
-    keyCode: 68,
-    keyTrigger: "D",
-    id: "Closed-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
-}, {
-    keyCode: 90,
-    keyTrigger: "Z",
-    id: "Punchy-Kick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
-}, {
-    keyCode: 88,
-    keyTrigger: "X",
-    id: "Side-Stick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
-}, {
-    keyCode: 67,
-    keyTrigger: "C",
-    id: "Snare",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
-}]
 
 function App() {
   return (
     <div className="App">
       <div className="text-center">
       <header className="App-header" >
-        <p>
-          Drum Machine
-        </p>
+        <h2>Drum Machine</h2>
         {audioClips.map(clip=>{
           <Pad key={clip.id} clip={clip}/>
         } )}
