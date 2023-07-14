5bdcc99a636a63db38e5202c1f3906dc635a2413
Zmulvihill <zmulvihi@yahoo.com>
2023-07-13 22:29:12 -0700

    added import logo, react, and export and saved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-const audioClips=[{
+
+const audioClips= [
+  {
     keyCode: 81,
     keyTrigger: "Q",
     id: "Heater-1",
@@ -49,33 +51,105 @@ const audioClips=[{
 }]
 
 function App() {
+  const [volume, setVolume] = React.useState(1);
+  const [recording, setRecording] = React.useState("");
+  const [speed, setSpeed] = React.useState(0.5);
+
+  const playRecording = () => {
+    let index = 0;
+    let recordArray = recording.split(" ");
+    const interval = setInterval(() => {
+      const audioTag = document.getElementById(recordArray[index]);
+      audioTag.volume = volume;
+      audioTag.currentTime = 0;
+      audioTag.play();
+      index++;
+    }, speed * 600);
+    setTimeout(() => clearInterval(interval), 600 * speed * recordArray.length - 1);
+  };
+
   return (
-    <div className="App">
+    <div className="bg-info min-vh-100 text-white">
       <div className="text-center">
-      <header className="App-header" >
         <h2>Drum Machine</h2>
-        {audioClips.map(clip=>{
-          <Pad key={clip.id} clip={clip}/>
-        } )}
-      </header>
+        {audioClips.map((clip) => (
+          <Pad
+            key={clip.id}
+            clip={clip}
+            volume={volume}
+            setRecording={setRecording}
+          />
+        ))}
+        <br />
+        <h4>Volume</h4>
+        <input
+          type="range"
+          step="0.01"
+          onChange={(e) => setVolume(e.target.value)}
+          value={volume}
+          max="1"
+          min="0"
+          className="w-50"
+        />
+        <h3>(recording)</h3>
+        {recording && (
+          <>
+            <button onClick={playRecording} className="btn btn-success">
+              Play
+            </button>
+            <button onClick={() => setRecording("")} className="btn btn-danger">
+              Clear
+            </button>
+            <br />
+            <h4>Speed</h4>
+            <input
+              type="range"
+              step="0.01"
+              onChange={(e) => setSpeed(e.target.value)}
+              value={speed}
+              max="1.2"
+              min="0.1"
+              className="w-50"
+            />
+          </>
+        )}
+      </div>
     </div>
+  );
+}
+
+function Pad({ clip, volume, setRecording }) {
+  const [active, setActive] = React.useState(false);
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  });
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === clip.keyCode) {
+      playSound();
+    }
+  };
+
+  const playSound = () => {
+    const audioTag = document.getElementById(clip.keyTrigger);
+    setActive(true);
+    setTimeout(() => setActive(false), 200);
+    audioTag.volume = volume;
+    audioTag.currentTime = 0;
+    audioTag.play();
+    setRecording((prev) => prev + clip.keyTrigger + " ");
+  };
+
+  return (
+    <div onClick={playSound} className="btn btn-secondary p-4 m-3">
+      <audio className="clip" id={clip.keyTrigger}src={clip.url} />
+      {clip.keyTrigger}
     </div>
   );
 }
 
 export default App;
-
-function  Pad({ clip }) {
-const playSound = () =>{
-  const audioTag = document.getElementById(clip.keyTrigger);
-  audioTag.currentTime = 0;
-  audioTag.play();
-};
-
-  return (
-        <div onClick={playSound} className= "btn btn-secondary p-4 m-3">
-          <audio className="clip" id={clip.keyTrigger} src={clip.url} />
-          {clip.keyTrigger}
-        </div>
-  );
-}
